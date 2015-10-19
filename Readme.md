2466af799cd3602d0a8252510c076373ef8df039
Soni Pandey <sonipandey.71@gmail.com>
2015-10-19 14:03:41 +0530

    updated

---
+++ Readme.md
@@ -0,0 +1,7 @@
+Angular UI Router Biolerplate
+
+This is the basic structure to start work in angular.
+
+### Run this application
+
+<pre>python -m SimpleHTTPServer</pre>
--- modules/app/controllers/app.client.controller.js
+++ modules/app/controllers/app.client.controller.js
@@ -1,10 +1,9 @@
 'use strict';
 
 
-angular.module('app').controller('AppController', ['$scope', 'Authentication',
-	function($scope, Authentication) {
+angular.module('app').controller('AppController', ['$scope',
+	function($scope) {
 		// This provides Authentication context.
-		$scope.authentication = Authentication;
 		$scope.demoText = "Hello Angular Ui Router";
 	}
 ]);
